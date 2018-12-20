480291f27926ce370946ed7c49f4af8b9d2f871d
M3roof <43769947+M3roof@users.noreply.github.com>
2018-12-20 15:14:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,38 @@ client.on('ready', () => {
 
 
 
+client.on('ready', async() => {
+var server = "516364082127831040"; // ايدي السررفر
+var channel = "524038276999413775";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    },305);
+})
 
+
+
+ 
+
+
+
+    client.on('message', message => {
+        var prefix = "2";
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+      
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+      
+      
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + 'say')) {
+            message.channel.send(''+x);
+                message.delete(999)
+        }
+        
+       
+      });
 
 
 
